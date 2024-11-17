4dd50ba8b35fd9aa95eafa75c66cac11bb777bda
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-11-17 20:18:54 +0530

    Update T28 friend fng.cpp

--- T28 friend fng.cpp
+++ T28 friend fng.cpp
@@ -1,58 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Complex; // Forward declaration ki baad mai use karenge abhi jaan lo ki ye ayega aage
+class Complex; // Forward declaration of Complex class
 
 class Cal {
 public:
     int add(int a, int b) {
-        return (a + b);
+        return a + b;
     }
     int real(Complex, Complex);
     int comp(Complex, Complex);
 };
 
 class Complex {
-    int a, b; // Private by default
-    friend int Cal::real(Complex, Complex); // likhit mai de diye int type ka cal agar call karege real fng ko toh allow kardena 
-    friend int Cal::comp(Complex, Complex); // likhit mai de diye int type ka cal agar call karege comp fng ko toh allow kardena 
+    int a, b; // Real and imaginary parts
+
+    // Grant access to Cal class methods
+    friend int Cal::real(Complex, Complex);
+    friend int Cal::comp(Complex, Complex);
 
 public:
     void set(int n, int m) {
         a = n;
         b = m;
     }
-    void display() {
+    void display() const {
         cout << a << " + " << b << "i" << endl;
     }
 };
 
-int Cal::comp(Complex A, Complex B) //uper define kiya tha idhar bana diya
-{
-    return (A.b + B.b);
+// Member function of Cal to compute the sum of imaginary parts
+int Cal::comp(Complex A, Complex B) {
+    return A.b + B.b;
 }
 
-int Cal::real(Complex A, Complex B) //uper define kiya tha idhar bana diya
-{
-    return (A.a + B.a);
+// Member function of Cal to compute the sum of real parts
+int Cal::real(Complex A, Complex B) {
+    return A.a + B.a;
 }
 
 int main() {
-    Complex c1, c2;
+    Complex c1, c2,c3;
+
+    // Set values for c1 and c2
     c1.set(1, 2);
     c2.set(3, 4);
 
     Cal c;
+
+    // Compute the sum of real and imaginary parts
     int real_sum = c.real(c1, c2);
     int comp_sum = c.comp(c1, c2);
-
+    c3.set(real_sum,comp_sum);
+    // Display the results
     cout << "Complex number 1: ";
     c1.display();
     cout << "Complex number 2: ";
     c2.display();
-
     cout << "Sum of real parts: " << real_sum << endl;
     cout << "Sum of imaginary parts: " << comp_sum << endl;
-
+    cout << "Complex number after sum: ";
+    c3.display();
+    
     return 0;
 }
