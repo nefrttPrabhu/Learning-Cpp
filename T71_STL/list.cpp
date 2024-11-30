4a253fe79fe0745ae47c6f29fdbc072a1f2dd5e7
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-12-01 01:21:29 +0530

    Create list.cpp

---
+++ T71_STL/list.cpp
@@ -0,0 +1,12 @@
+/*
+  --> they uses doubly linked list for storage due to which they have O(1) for insertions better than vectors
+*/
+
+1.  list<int> ls;  // Create a list of integers
+    // Add elements at the front
+    ls.push_front(5);
+    ls.emplace_front(2);
+
+    // Add elements at the back
+    ls.push_back(10);
+    ls.emplace_back(15);
